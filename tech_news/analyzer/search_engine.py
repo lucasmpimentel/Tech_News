0e9a362674791d0bc35ed3a47d301fb6d3877759
Lucas Pimentel <lucas.mirandapimentel@gmail.com>
2022-10-22 19:47:45 -0300

    req 6

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,15 @@
+from tech_news.database import search_news
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    lista_noticias = []
+
+    busca = search_news({"title": {"$regex": title, "$options": "i"}})
+
+    for noticias in busca:
+        lista_noticias.append((noticias["title"], noticias["url"]))
+    return lista_noticias
 
 
 # Requisito 7
