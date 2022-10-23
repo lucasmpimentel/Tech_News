1c3c537537d0b56b01616d86d9c5067092b0d215
Lucas Pimentel <lucas.mirandapimentel@gmail.com>
2022-10-22 21:03:26 -0300

    req 8

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+from datetime import datetime
 
 
 # Requisito 6
@@ -7,19 +8,38 @@ def search_by_title(title):
 
     busca = search_news({"title": {"$regex": title, "$options": "i"}})
 
-    for noticias in busca:
-        lista_noticias.append((noticias["title"], noticias["url"]))
+    for noticia in busca:
+        lista_noticias.append((noticia["title"], noticia["url"]))
     return lista_noticias
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    result = []
+
+    try:
+        data = datetime.strptime(date, "%Y-%m-%d").strftime("%d/%m/%Y")
+        lista_noticias = search_news({"timestamp": data})
+
+        for noticia in lista_noticias:
+            result.append((noticia["title"], noticia["url"]))
+
+        return result
+
+    except (ValueError):
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
 def search_by_tag(tag):
-    """Seu código deve vir aqui"""
+    lista_noticias = []
+
+    busca = search_news({"tags": {"$regex": tag, "$options": "i"}})
+
+    for noticia in busca:
+        lista_noticias.append((noticia["title"], noticia["url"]))
+
+    return lista_noticias
 
 
 # Requisito 9
