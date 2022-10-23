cee9b34140b161cbf7fce201dc5e5ecbe21c46bc
Lucas Pimentel <lucas.mirandapimentel@gmail.com>
2022-10-22 22:07:43 -0300

    req 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,22 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    result = []
+
+    busca = sorted(
+        sorted(find_news(), key=(lambda el: el["title"])),
+        reverse=True,
+        key=(lambda el: el["comments_count"])
+    )[:5]
+
+    if busca:
+        for noticia in busca:
+            titulo = noticia.get("title")
+            url = noticia.get("url")
+            result.append((titulo, url))
+    return result
 
 
 # Requisito 11
