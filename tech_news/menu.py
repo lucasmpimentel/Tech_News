420201658dabb68655ebe7e3300ce5609e686473
Lucas Pimentel <lucas.mirandapimentel@gmail.com>
2022-10-25 22:46:14 -0300

    fix: identação

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -5,16 +5,16 @@ import sys
 def analyzer_menu():
     entrada = input(
         """
-        Selecione uma das opções a seguir:
-            0 - Popular o banco com notícias;
-            1 - Buscar notícias por título;
-            2 - Buscar notícias por data;
-            3 - Buscar notícias por tag;
-            4 - Buscar notícias por categoria;
-            5 - Listar top 5 notícias;
-            6 - Listar top 5 categorias;
-            7 - Sair.
-        """
+    Selecione uma das opções a seguir:
+ 0 - Popular o banco com notícias;
+ 1 - Buscar notícias por título;
+ 2 - Buscar notícias por data;
+ 3 - Buscar notícias por tag;
+ 4 - Buscar notícias por categoria;
+ 5 - Listar top 5 notícias;
+ 6 - Listar top 5 categorias;
+ 7 - Sair.
+    """
     )
 
     resposta = [
